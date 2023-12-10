d5bb3369a2f4985decfc5914b22370f667864c97
kanhaji <myself.kanhaji@gmail.com>
2023-12-10 20:06:39 +0530

    v2 continue

--- src/app.js
+++ src/app.js
@@ -1,70 +1,91 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-// db url
-const appSettings = {
-    databaseURL : "https://playgroung-kanhaji-default-rtdb.asia-southeast1.firebasedatabase.app/",
-}
-// Initialize App
-const app = initializeApp(appSettings)
-const DB = getDatabase(app)
-const itemInDb = ref(DB, "BuketList")
-// Get value from DOM
-const inputFild = document.getElementById("input-field")
-const btn = document.getElementById("add-button")
-const buketList = document.getElementById("items")
+const form = document.getElementById("cart-form");
+const items = document.getElementById("items");
+const button = document.getElementById("add-button");
 
-// geting data from firebase
-// onValue is firebase function to get our data from firebase-db
-onValue(itemInDb,  (snapshot) => {
-    // snapsshot.exists() methos will return true if any itam is present in our db else return false
-    if(!snapshot.exists()) {
-        buketList.innerHTML = "No item at present ... ¯\_(ツ)_/¯"
-        return
+const followSwipe = (container, handleSwipe) => {
+  let startX = null;
+  let startY = null;
+
+  container.addEventListener("touchstart", (event) => {
+    startX = event.changedTouches[0].clientX;
+    startY = event.changedTouches[0].clientY;
+  });
+
+  container.addEventListener("touchend", (event) => {
+    const endX = event.changedTouches[0].clientX;
+    const endY = event.changedTouches[0].clientY;
+
+    const swipeDistanceX = Math.abs(startX - endX);
+    const swipeDistanceY = Math.abs(startY - endY);
+
+    let swipeDirection;
+    if (swipeDistanceX > swipeDistanceY && swipeDistanceX > 50) {
+      swipeDirection = startX < endX ? "right" : "left";
+    } else if (swipeDistanceY > swipeDistanceX && swipeDistanceY > 50) {
+      swipeDirection = startY < endY ? "down" : "up";
     }
-    /*obj to arry
-    * Object.entries(obj) > [[key, value],...]
-    * Object.values(obj) > [values,...]
-    * Object.keys(obj) > [key,...]
-    */ 
-    let data =  Object.entries(snapshot.val())
-    buketList.innerHTML = ""
-    data.map((item) => addItem(item))
-})
 
-// Function for click even and Enter press envet
-function click() {
-    const inputValue = inputFild.value
-    // push is firebase fun to push new item to db
-    push(itemInDb ,inputValue)
-    // clear the input fild for new entry
-    inputFild.value = ''
-}
+    // Handle swipe direction
+    handleSwipe(swipeDirection);
+  });
+};
 
-// Event Listener for Enter Key
-document.addEventListener('keydown', (event) => {
-    var name = event.key;
-    // var code = event.code;
-    if(name == 'Enter') click()
-}, false);
+let list = JSON.parse(localStorage.getItem("cart-list")) || [];
 
-// Event Listener for Butten Click
-btn.addEventListener("click", click)
+const append = (item) => {
+  list.push(item);
+  localStorage.setItem("cart-list", JSON.stringify(list));
+};
 
-// Function to add item to unorder list in DOM
-function addItem(item) {
-    let [id, data] = [item[0], item[1]]
-    // add to Buket list
-    // buketList.innerHTML += `<li>${date}</li>`
-    let li =  document.createElement("li")
-    li.textContent = data
-    li.addEventListener("dblclick", () => {removeItem(id)})
+const remove = (index) => {
+  list = list.splice(index, 1);
 
-    buketList.append(li)
-}
+  localStorage.setItem("cart-list", JSON.stringify(list));
+  renderItems();
+  console.log(list);
+};
 
-// Function to remove item
-function removeItem(id) {
-    let delItem = ref(DB, `BuketList/${id}`)
-    // `remove` is a fun to remove data from firebase
-    remove(delItem)
-}
+const renderItems = () => {
+  items.innerHTML = "";
+
+  list.forEach((item) => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    li.id = `${list.length}`;
+
+    li.addEventListener("dblclick", (e) => {
+      const index = li.id;
+
+      remove(index);
+    });
+
+    followSwipe(li, (direction) => {
+      if (direction == "right") {
+        const index = li.id;
+
+        remove(index);
+      }
+    });
+
+    items.append(li);
+  });
+};
+
+renderItems();
+
+const onSubmit = () => {
+  const item = document.getElementById("input-field").value;
+
+  append(item);
+  renderItems();
+  console.log(list);
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  onSubmit();
+});
+
+button.addEventListener("click", () => {
+  onSubmit();
+});
